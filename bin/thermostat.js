410f05009400df819718ea42ee6c06bf36e7515c
Mike Green <mike.is.green@gmail.com>
2022-01-27 10:32:40 -0600

    Skeleton of App controller complete. Executable runs a loop checking sensors

--- bin/thermostat.js
+++ bin/thermostat.js
@@ -1,37 +1,9 @@
-const App = require('../lib/app')
+const AppController = require('../lib/app')
+const app = new AppController({ relays: [25, 26] })
 
-const relay1 = new Gpio(25, 'out')
-const relay2 = new Gpio(26, 'out')
+app.start()
 
-const sensorDir = '/sys/bus/w1/devices'
-
-async function resetRelays() {
-  await Promise.all([relay1.write(1), relay2.write(1)])
-}
-
-async function getSensors() {
-  const dir = await readdir(sensorDir)
-  const devices = dir.filter((child) => child.startsWith('28-'))
-
-  return devices
-}
-
-async function readSensor(sensor) {
-  const temp = path.join(sensorDir, sensor, 'temperature')
-  const value = await readFile(temp)
-  const degreesC = parseInt(value, 10) / 1000
-  const degreesF = degreesC * (9 / 5) + 32
-
-  return degreesF
-}
-
-async function main() {
-  await resetRelays()
-
-  const [sensor] = await getSensors()
-  const reading = await readSensor(sensor)
-
-  console.log('Temp reading: %s', reading)
-}
-
-main()
+process.on('SIGINT', () => {
+  console.log('Stopping main loop and exiting')
+  app.stop()
+})
